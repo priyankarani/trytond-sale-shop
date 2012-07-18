872fd884e5c579874bf7da2d5bbd9635859fdb76
resteve <resteve@zikzakmedia.com>
2012-07-18 11:09:53 +0200

    Change copyright notice files

--- __tryton__.py
+++ __tryton__.py
@@ -1,5 +1,6 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+#This file is part sale_shop module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 {
     'name': 'Sale Shop',
     'name_ca_ES': 'Vendes per botiga',
